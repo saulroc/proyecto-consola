3c9d94ac612c21441ec68d5872193e4f9438fc3c
Carlos Pareja <saulroc@hotmail.com>
2022-02-03 08:33:51 +0100

    Iniciamos el proyecto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensajeDeSalida;
+
+            mensajeDeSalida = "Hello Mora!";
+            Console.WriteLine(mensajeDeSalida);
+
+            mensajeDeSalida = "Estamos programando";
+            Console.Write(mensajeDeSalida);
+            mensajeDeSalida = Console.ReadLine();
+            Console.WriteLine("Tu mensaje es:" + mensajeDeSalida);
+            Console.ReadLine(); //Esto es una pausa
+             
+            
+        }
+    }
+}
