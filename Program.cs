4ea4cac899a8ef2c9d8dc503c8b0a6496e4a835e
Carlos Pareja <saulroc@hotmail.com>
2022-02-14 12:17:15 +0100

    Añadimos un try catch finally

--- Program.cs
+++ Program.cs
@@ -8,17 +8,36 @@ namespace consola
         const int NUMERO_ENTRADA_INICIAL = 1;
         static void Main(string[] args)
         {
-            MiSegundaClase miSegundaClase = new MiSegundaClase();
-            miSegundaClase.Mensaje = MENSAJE_INICIAL;
-            
-            miSegundaClase.NumeroEntrada = NUMERO_ENTRADA_INICIAL;
-            miSegundaClase.IncremetarCinco();
-            miSegundaClase.PintarLetras2();
-            miSegundaClase.Escribir();
-
-            Console.WriteLine(MENSAJE_INICIAL);
-
-            Console.ReadKey();
+            Console.WriteLine("Introduzca un numero: ");
+            int numeroIntroducido;
+            try
+            {
+                numeroIntroducido = Int32.Parse(Console.ReadLine());  
+                Console.WriteLine("El numero introducido es: " + numeroIntroducido.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error genérico");
+                // throw ex;
+            }
+            // catch (System.FormatException ex)
+            // {
+            //     Console.WriteLine("El valor introducido no tiene formato de número");
+            //     throw ex;
+            // }
+            // catch (System.OverflowException)
+            // {
+            //     Console.WriteLine("El número introducido es demasiado grande o demasiado pequeño");
+            // }
+            // catch(System.ArgumentNullException)
+            // {
+            //     Console.WriteLine("No ha introducido un valor");
+            // }
+            finally
+            {
+                Console.WriteLine("Esto se ejecuta siempre");
+            }
+            Console.ReadLine();
         }
     }
     
