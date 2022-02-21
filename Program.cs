ee46eb98066ab8a854787e8549a6a1311fba6540
Carlos Pareja <saulroc@hotmail.com>
2022-02-21 12:47:11 +0100

    Ejemplo de if else con ? : y herencia

--- Program.cs
+++ Program.cs
@@ -8,38 +8,35 @@ namespace consola
         const int NUMERO_ENTRADA_INICIAL = 1;
         static void Main(string[] args)
         {
-            Console.WriteLine("Introduzca una opción: ");
-            int opcion = Int32.Parse(Console.ReadLine());
-            // if (opcion == 1)
-            // {
-            //     //hago algo
-            // }
-            // else if (opcion == 2)
-            // {
-            //     // hago otra cosa
-            // }
-            // else if (opcion == 3)
-            // {
-            //     //otra operación distinta
-            // }
-            // else
-            // {
-            //     //operación por defecto
-            // }
-            switch (opcion)
-            {
-                case 1:
-                case 3:
-                case 4:
-                    //hacer algo
-                    break;
-                case 2:
-                    // hacer otra cosa
-                    break;
-                default:
-                    // operación por defecto
-                    break;
-            }
+            int opcion = 1;
+            
+            // CONDICION ? verdadero : falso;
+            string mensaje = opcion == 1 
+                ? "Es opción 1" 
+                : "No es opción 1";
+
+            //return opcion == 1 ? "Es opción 1" : "No es opción 1";
+            //return opcion == 1 || opcion == 2 ? "Es opción 1" : "No es opción 1";
+
+            Console.WriteLine(mensaje);
+
+            var vehiculo = new Vehiculo(80);
+            Console.WriteLine("Mi límite de velocidad es: " + vehiculo.LimiteMaximoDeVelocidad);
+            vehiculo = new Turismo();
+            Console.WriteLine("Mi nuevo límite de velocidad es: " + vehiculo.LimiteMaximoDeVelocidad);
+
+            Vehiculo[] vehiculos = new Vehiculo[2];
+            vehiculos[0] = new Camion();
+            vehiculos[1] = new Turismo();
+
+            vehiculos[0].Matricula = "0000AAA";
+            Console.WriteLine("La matrícula es: " + vehiculos[0].Matricula);
+            
+            //Cast -> Conversión de un tipo genérico a un tipo específico
+            Camion camion = (Camion)vehiculos[0];
+            camion.CapacidadDeCarga = 1000;
+            Console.WriteLine("La capacidad de carga es: " + camion.CapacidadDeCarga);
+
             Console.ReadLine();
         }
     }
