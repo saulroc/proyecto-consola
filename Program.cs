c2fbbbf3c2d43c28b936da12804aa7d1a811dd86
Carlos Pareja <saulroc@hotmail.com>
2022-03-07 12:29:38 +0100

    Actualizado el program con datetime

--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
-using IESPeñasNegras.ProyectoEjemplo.Consola.Clases;
 
 namespace IESPeñasNegras.ProyectoEjemplo.Consola
 {
-    
+
     class Program
     {
          static void Main(string[] args)
         {
-            var miPrimeraClase = new MiPrimeraClase();
-            var miSegundaClase = new MiSegundaClase();
-            MiTerceraClase miTerceraClase = new MiTerceraClase();
+            //declaración e instancia de las variables
+            var fechaActual = DateTime.Now;
+            var fecha = new DateTime(1992, 10, 13);
             
+            //uso de método
+            Console.WriteLine(fechaActual.AddYears(-10));
+            //fechaActual = fechaActual.AddYears(-10);
+            //Propiedad Date
+            Console.WriteLine(fechaActual.Date);
+            Console.WriteLine(fechaActual.Date.ToString("dd-MM-yyyy"));
+            
+            //Parte time
+            Console.WriteLine(fechaActual.TimeOfDay);
+
             Console.ReadLine();
         }
         
