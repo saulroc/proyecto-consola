3a16d87327dba36aead1cde3d0a2e469ac121552
Carlos Pareja <saulroc@hotmail.com>
2022-03-16 13:33:46 +0100

    Añadidas expresiones lambda

--- Program.cs
+++ Program.cs
@@ -8,28 +8,39 @@ namespace IESPeñasNegras.ProyectoEjemplo.Consola
 
     class Program
     {
-         static void Main(string[] args)
+         static async void Main(string[] args)
         {
-            //Genericos <T> de tipo string
-            List<string> mensajes = new List<string>();
-            mensajes.Add("Un mensaje");
-            string miMensaje = "¡Hola mundo!";
+            List<Turismo> turismos = new List<Turismo>() {
+                new Turismo() { Modelo = "Seat" },
+                new Turismo() { Modelo = "Peugeot" },
+                new Turismo() { Modelo = "Opel"}
+            };
 
-            //Genericos <T> donde T es IVehiculo de tipo Turismo
-            var listaVehiculos = new ListaVehiculos<Turismo>();
-            listaVehiculos.AñadirVehiculo(new Turismo());
-
-            var procesadorDeFotos = new ProcesadorDeFotos();
-            var fotoFiltros = new FotoFiltros();
-            Action<Foto> filtrosHandler = fotoFiltros.AplicarBrillo;
-            filtrosHandler += fotoFiltros.AjustarTamaño;
-            filtrosHandler += fotoFiltros.AplicarContraste;
-            filtrosHandler += QuitarOjosRojos;
+            turismos.Find(EncontrarPeugeot);
             
-            procesadorDeFotos.Procesar("", filtrosHandler);
-
+            Func<Turismo, bool> encontrarTodos = t => t.Modelo == "Peugeot";
+            //turismos.FindAll(encontrarTodos);
+            turismos.FindAll(t => t.Modelo == "Peugeot" || t.Modelo == "Seat");
+            
+            Func<int, int> square = x => x * x;
+            Console.WriteLine(square(5));
+            
             Console.ReadLine();
         }  
+
+        public static bool EncontrarPeugeot(Turismo turismo)
+        {
+            // if (turismo.Modelo == "Peugeot")
+            // {
+            //     return true;
+            // }
+            // else 
+            // {
+            //     return false;
+            // }
+            Console.WriteLine(turismo);
+            return turismo.Modelo == "Peugeot";
+        }
 
         public static void QuitarOjosRojos(Foto foto)    
         {
