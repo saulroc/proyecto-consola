968df04154f782b881b47138fa2595e010e76e23
Carlos Pareja <saulroc@hotmail.com>
2022-03-29 09:01:23 +0200

    Añadido proyecto mvc

---
+++ MVC/Controllers/VehiculosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class VehiculosController : Controller
+    {
+        public IActionResult Index()
+        {
+            var vehiculo = new Vehiculo(1, "Ford Scort");
+
+            var vehiculo2 = new Vehiculo();
+            vehiculo2.Id = 2;
+            vehiculo2.Name = "Ford Fiesta";
+
+            var vehiculo3 = new Vehiculo() { Id = 3, Name = "Ford Focus" };
+
+            return View(vehiculo);
+        }
+    }
+}
---
+++ MVC/Models/Vehiculo.cs
@@ -0,0 +1,18 @@
+﻿namespace MVC.Models
+{
+    public class Vehiculo
+    {
+        public Vehiculo(int id, string name)
+        {  
+            Id = id;
+            Name = name;
+        }
+
+        public Vehiculo()
+        {
+
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
