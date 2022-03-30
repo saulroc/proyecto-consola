fe7ab18f8b90acd94cd2bbd6d5fa4ccbf0822420
Carlos Pareja <saulroc@hotmail.com>
2022-03-30 14:19:16 +0200

    Añadida la migración

---
+++ consola/Migrations/20220330120358_InitialCreate.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace consola.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Camiones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CapacidadDeCarga = table.Column<int>(nullable: false),
+                    Tipo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Camiones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Turismos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tipo = table.Column<string>(nullable: true),
+                    Modelo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Turismos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ruedas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Diametro = table.Column<int>(nullable: false),
+                    TurismoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ruedas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ruedas_Turismos_TurismoId",
+                        column: x => x.TurismoId,
+                        principalTable: "Turismos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ruedas_TurismoId",
+                table: "Ruedas",
+                column: "TurismoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Camiones");
+
+            migrationBuilder.DropTable(
+                name: "Ruedas");
+
+            migrationBuilder.DropTable(
+                name: "Turismos");
+        }
+    }
+}
