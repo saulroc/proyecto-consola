4eee07c85bea9dc6abe46c74ab7b50d0e716c7bf
Carlos Pareja <saulroc@hotmail.com>
2022-03-31 10:07:10 +0200

    Añadidas operaciones de lectura, actualización y borrado

--- consola/Program.cs
+++ consola/Program.cs
@@ -14,9 +14,24 @@ namespace IESPeñasNegras.ProyectoEjemplo.Consola
           
             using(var dbContext = new VehiculosContext())
             {
-                System.Console.WriteLine("Insertando un turismo");
-                dbContext.Add(new Turismo(default) { Modelo = "Ford Focus"});
+                // System.Console.WriteLine("Insertando un turismo");
+                // dbContext.Add(new Turismo(default) { Modelo = "Ford Cugart"});
+                // dbContext.SaveChanges();
+
+                // var turismo = dbContext.Set<Turismo>().Where(t => t.Modelo == "Ford Scort").FirstOrDefault();
+                // System.Console.WriteLine(turismo);
+
+                // turismo.Modelo = "Nuevo Modelo";
+                // dbContext.SaveChanges();
+
+                var turismo = dbContext.Set<Turismo>().Where(t => t.Modelo == "Nuevo Modelo").FirstOrDefault();
+                System.Console.WriteLine(turismo);
+
+                System.Console.WriteLine("Borrando el turismo");
+                dbContext.Remove(turismo);
                 dbContext.SaveChanges();
+
+
             }
             Console.ReadLine();
         }   
